e94718fcb18aaeee8770b25285b51fe24adb33fa
VikasKurella <vikaschandrakurella@gmail.com>
2022-08-26 13:13:10 -0500

    store notes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 class App {
 	constructor() {
+		this.notes = []
 		this.$form = document.querySelector('#form')
 		this.$noteTitle = document.querySelector('#note-title')
+		this.$noteText = document.querySelector('#note-text')
 		this.$formButtons = document.querySelector('#form-buttons')
 		this.addEventListeners();
 	}
@@ -10,6 +12,16 @@ class App {
 		document.body.addEventListener('click', event => {
 			this.handleFormClick(event);
 		});
+		
+		this.$form.addEventListener('submit', event => {
+			event.preventDefault();
+			const title = this.$noteTitle.value;
+			const text = this.$noteText.value;
+			const hasNote = title || text
+			if (hasNote) {
+				this.addNote({ title, text })
+			}
+		})
 	}
 	
 	handleFormClick(event) {
@@ -31,6 +43,17 @@ class App {
 		this.$form.classList.remove('form-open');
 		this.$noteTitle.style.display = 'none';
 		this.$formButtons.style.display = 'none';
+	}
+	
+	addNote(note) {
+		const newNote = {
+			title: note.title,
+			text: note.text,
+			color: 'white',
+			id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+		}
+		this.notes = [...this.notes, newNote]
+		console.log(this.notes)
 	}
 }
 
